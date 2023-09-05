4bb73eac1b38366e313b16182a965fa6e9eb6702
donertester <144096290+donertester@users.noreply.github.com>
2023-09-05 15:30:29 +0500

    Update ChartExamples.py
    Dobavleniye knopki zakrip

--- ChartExamples.py
+++ ChartExamples.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 
+def do_close():
+    window.destroy()
+    
 
 window = tk.Tk()
 window.geometry("450x450")
 window.title("Примеры построение графиков")
 
+btnClose=tk.Button(window,text="Close", font =('Helvetica',10,'bold'), command=do_close)
+btnClose.place(x=330,y=400,width=90,height=30)
 window.mainloop()
